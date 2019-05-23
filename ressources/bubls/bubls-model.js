57af089d806e4a32a0f72b79710dc6953f2d648f
guillsav <guillsav@gmail.com>
2019-05-23 03:44:55 -0400

    change role, join school, join bubl works locally

--- ressources/bubls/bubls-model.js
+++ ressources/bubls/bubls-model.js
@@ -21,12 +21,12 @@ function getBublById(id) {
     .first();
 }
 
-function getBublUsers(id) {
-  return db('bubl_users_mapping')
-    .join('users', 'users.id', 'bubl_users_mapping.FK_users_id')
-    .select('users.id', 'users.username')
-    .where('bubl_users_mapping.FK_users_id', id)
-    .then(users => users.map(user => mapper.userToBody(user)));
+function getBublUsers(bublId) {
+  const id = parseInt(bublId);
+  return db('users as u')
+    .join('bubl_users_mapping as m', 'm.FK_users_id', 'u.id')
+    .select('u.id', 'u.username', 'u.role')
+    .where({FK_bubl_id: id});
 }
 
 async function addBubl(bubl) {
@@ -47,10 +47,20 @@ function deleteBubl(id) {
     .del();
 }
 
-async function joinBubl(userId, bublId) {
-  const [id] = await db('bubl_users_mapping').insert({
-    FK_users_id: userId,
-    FK_bubl_Id: bublId
-  });
-  return db('bubl_users_mapping').where('id', id);
+function joinBubl({subject: FK_users_id}, {FK_bubl_id}) {
+  return db('bubl_users_mapping')
+    .where({FK_users_id})
+    .insert({
+      FK_users_id,
+      FK_bubl_id
+    });
 }
+
+// function joinSchool(school, user) {
+//   return db('users')
+//     .where({id: user.subject})
+//     .update({
+//       school_name: school.school_name,
+//       FK_school_id: school.FK_school_id
+//     });
+// }
